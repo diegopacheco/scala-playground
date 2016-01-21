c2b598fc59dd4924cd1c6b938d924b943ee80ecb
diegopacheco <diego.pacheco.it@gmail.com>
2016-01-21 10:14:32 -0200

    .
    .

--- scala-some-fp/src/main/scala/com/github/diegopacheco/scala/playground/fp/TrySucessFailApp.scala
+++ scala-some-fp/src/main/scala/com/github/diegopacheco/scala/playground/fp/TrySucessFailApp.scala
@@ -12,16 +12,16 @@ object TrySucessFailApp extends App {
     }
   }
   
-  println( "1. " + guessMyName("Diego").getOrElse(":(") )
+  println( "1. " + guessMyName("Diego").getOrElse(":(") ) // 1. :(
   
   try{
-    println( "2. " + guessMyName("Diego").get )  
+    println( "2. " + guessMyName("Diego").get )  // 2. Ops : Wrong name
   }catch {
     case e:Exception => println("2. Ops : " + e.getMessage);  
   }
   
-  println( "3 " + guessMyName("Diego").map { x => x.toUpperCase() } )
+  println( "3 " + guessMyName("Diego").map { x => x.toUpperCase() } ) // 3 Failure(java.lang.IllegalArgumentException: Wrong name)
   
-  println( "4 " + guessMyName("heisenberg").map { x => x.toUpperCase() } )
+  println( "4 " + guessMyName("heisenberg").map { x => x.toUpperCase() } ) // 4 Success(THE KING)
   
 }
