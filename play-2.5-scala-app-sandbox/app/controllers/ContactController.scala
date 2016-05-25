c2899e8e9ab730d50b87e4a4416ae612c1438e55
Diego Pacheco <diego.pachecoit@gmail.com>
2016-05-24 23:52:04 -0700

    some code with form - not fully complete

--- play-2.5-scala-app-sandbox/app/controllers/ContactController.scala
+++ play-2.5-scala-app-sandbox/app/controllers/ContactController.scala
@@ -7,21 +7,63 @@ import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data._
 import models.Contact
+import play.api.i18n.Messages
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
 
 @Singleton
-class ContactController @Inject() extends Controller {
-  
+class ContactController @Inject() (val messagesApi:MessagesApi) extends Controller with I18nSupport{
+
   val contactForm: Form[Contact] = Form(
     mapping(
       "firstname" -> nonEmptyText,
       "lastname" -> nonEmptyText,
-      "company" -> optional(text)
-    )(models.Contact.apply)(models.Contact.unapply)
- )
- 
-  def form = Action { implicit request =>
-    val product = contactForm.bindFromRequest.get
-    Ok("product = " + product.toString)
+      "company" -> optional(text))(models.Contact.apply)(models.Contact.unapply))
+
+  def index = Action { implicit request =>
+    Logger.info("index called. ")
+    Ok(views.html.contact_details(None, null))
   }
- 
+
+  def blank = Action { implicit request =>
+    Ok(views.html.contact_details(None, contactForm))
+  }
+
+  def details(id: Long) = Action { implicit request =>
+    Logger.info("details called. id: " + id)
+    val contact = Contact("", "", None)
+    Ok(views.html.contact_details(Some(id), contactForm.fill(contact)))
+  }
+
+  def insert()= Action { implicit request =>
+    Logger.info("insert called.")
+    contactForm.bindFromRequest.fold(
+      form => {
+        BadRequest(views.html.contact_details(None, form))
+      },
+      contact => {
+        Redirect(routes.ContactController.details(0)).flashing("success" -> Messages("success.insert", "PRODUCT_HARD_CODED"))
+      })
+  }
+
+  def update(id: Long) = Action { implicit request =>
+    Logger.info("updated called. id: " + id)
+    contactForm.bindFromRequest.fold(
+      form => {
+        Ok(views.html.contact_details(Some(id), form)).flashing("error" -> "Fix the errors!")
+      },
+      contact => {
+        //contact.update(id, product)
+        Redirect(routes.ContactController.details(0)).flashing("success" -> Messages("success.update", contact.firstname))
+      })
+  }
+
+  def delete(id: Long)= Action {
+    Redirect(routes.ContactController.details(0)).flashing("success" -> Messages("success.delete", id))
+//    Contacts.find(id).map { product =>
+//      Products.delete(id)
+//      Redirect(routes.Application.index).flashing("success" -> Messages("success.delete", product.name))
+//    }.getOrElse(NotFound)
+  }
+
 }
