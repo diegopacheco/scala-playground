9654bc704f952993d12b7ee8f374a8e3a28900a0
diegopacheco <diego.pacheco.it@gmail.com>
2016-07-27 01:06:35 -0700

    added stream and delete

--- scala-slick-fun/src/main/scala/com/github/diegopacheco/scala/slick/fun/mysql/HelloSlickMySQL.scala
+++ scala-slick-fun/src/main/scala/com/github/diegopacheco/scala/slick/fun/mysql/HelloSlickMySQL.scala
@@ -8,17 +8,26 @@ import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.concurrent.Future
+import slick.backend.DatabasePublisher
 
 object HelloSlickMySQL extends App {
   
   val pool = Executors.newCachedThreadPool()
   implicit val ec = ExecutionContext.fromExecutorService(pool)
   
+  //
+  // Setup DB Connection
+  //
+   
   val dbConfig = ConfigFactory.load().getConfig("db")
   Class.forName(dbConfig.getString("driver"))
   val db = Database.forConfig("db")
   println(db)
 
+  //
+  // Map Pojo to Tables
+  //
+  
   case class People(id: Int, name: String)
 
   class Peoples(tag: Tag) extends Table[(Int, String)](tag, "People") {
@@ -33,6 +42,10 @@ object HelloSlickMySQL extends App {
   val schema = peoples.schema
   println(schema)
   
+  //
+  // Creates the Schema
+  //
+  
   try{
     Await.result(db.run(DBIO.seq(
       schema.create
@@ -40,6 +53,10 @@ object HelloSlickMySQL extends App {
   }catch{
     case e:Exception => Unit
   }
+  
+  //
+  // Insert Data
+  //
   
   try{
     Await.result(db.run(DBIO.seq(
@@ -51,10 +68,36 @@ object HelloSlickMySQL extends App {
     case e:Exception => Unit
   }
   
+  //
+  // Search Data
+  //
+  
   val q = for (c <- peoples) yield c.name
   val a = q.result
   val f: Future[Seq[String]] = db.run(a)
   f.onSuccess { case s => println(s"Result: $s") }
+
+  //
+  // Delete
+  //
+  try{
+    Await.result(db.run(DBIO.seq(
+      peoples.filter(p => p.name === "John").delete
+    )),10 seconds)
+  }catch{
+    case e:Exception => Unit
+  }
+  
+  //
+  // Stream data
+  //
+  
+  val q2 = for (c <- peoples) yield c.name
+  val a2 = q2.result
+  val p2: DatabasePublisher[String] = db.stream(a)
+  p2.foreach { s => println(s"Element: $s") }
+  
+  Thread.sleep(3000)
   
   db.close()
   println("DONE")
