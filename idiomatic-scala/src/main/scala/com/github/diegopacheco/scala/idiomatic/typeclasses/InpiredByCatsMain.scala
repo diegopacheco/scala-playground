25f43777440e1c43690da051ca6905b792fa88c4
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-07-03 16:29:46 -0700

    Added TypeClass awesome sample

---
+++ idiomatic-scala/src/main/scala/com/github/diegopacheco/scala/idiomatic/typeclasses/InpiredByCatsMain.scala
@@ -0,0 +1,34 @@
+package com.github.diegopacheco.scala.idiomatic.typeclasses
+
+object InpiredByCats extends App {
+  
+  // Type Class = Type system functionality
+  trait Show[T]{
+    def show(value:T):String
+  }
+
+  // Type Instance = All Instances for Supported Types
+  object ShowInstances{
+    implicit val intInstance = new Show[Int] {
+      override def show(value:Int):String = value.toString()
+    }
+    implicit val StringInstance = new Show[String] {
+      override def show(value:String):String = s"-- ${value} --"
+    }
+  }
+  
+  // Type Interface - Functions for the Type Class instances
+  object ShowInterface{
+    def printWithShow[T](value:T)(implicit show:Show[T]):Unit = println( show.show(value) ) 
+  }
+  
+  // Beatiful Syntax Support
+  implicit class ShowSyntax[T](value:T){
+    def printWithShow(implicit show:Show[T]):Unit = println(show.show(value))
+  }
+  
+  import ShowInstances._
+  "Diego".printWithShow
+  42.printWithShow
+  
+}
