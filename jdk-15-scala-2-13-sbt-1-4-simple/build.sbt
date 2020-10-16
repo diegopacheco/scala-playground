8f3ad061fddd12eacf5114a7272236bc184e609d
diegopacheco <diego.pacheco.it@gmail.com>
2020-10-16 13:15:36 -0300

    working

---
+++ jdk-15-scala-2-13-sbt-1-4-simple/build.sbt
@@ -0,0 +1,3 @@
+name := "jdk-15-scala-2-13-sbt-1-4-simple"
+version := "1.0"
+scalaVersion := "2.13.3"
--- jdk-15-scala-2-13-sbt-1-4-simple/src/main/scala/com/github/diegopacheco/scala/main/MainApp.scala
+++ jdk-15-scala-2-13-sbt-1-4-simple/src/main/scala/com/github/diegopacheco/scala/main/MainApp.scala
@@ -2,4 +2,8 @@ package com.github.diegopacheco.scala.main
 
 object MainApp {
 
+  def main(args:Array[String]):Unit = {
+      println("JDK 15, Scala 2.13, Sbt 1.4.0 working! ")
+  }
+
 }
