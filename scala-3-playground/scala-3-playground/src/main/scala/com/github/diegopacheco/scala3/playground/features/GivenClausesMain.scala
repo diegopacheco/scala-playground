427131315a64073096e17b69bd8a0f11beda2010
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-14 02:38:36 -0300

    Playing with scala 3 - given clauses

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/GivenClausesMain.scala
@@ -0,0 +1,23 @@
+package com.github.diegopacheco.scala3.playground.features
+
+object GivenClausesMain extends App{
+
+  case class Config(port: Int, baseUrl: String)
+
+  def renderWebsite(path: String)(using Config): String =
+    "<html>" + renderWidget(List("cart")) + "</html>"
+
+  def renderWidget(items: List[String])(using Config): String = items.toString() 
+
+  val config = Config(8080, "docs.scala-lang.org")
+  given Config = config
+  
+  // option 1
+  // val result = renderWebsite("/home")(using config)
+
+  // option 2
+  val result = renderWebsite("/home")
+  
+  println(result)
+  
+}
--- scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/OpenClassMain.scala
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/OpenClassMain.scala
@@ -1,5 +1,21 @@
 package com.github.diegopacheco.scala3.playground.features
 
-object OpenClassMain {
+object OpenClassMain extends App{
 
+  open class Writer[T]:
+    /** Sends to stdout, can be overridden */
+    def send(x: T) = println(x)
+    /** Sends all arguments using `send` */
+    def sendAll(xs: T*) = xs.foreach(send)
+  end Writer
+
+  import scala.language.adhocExtensions
+
+  class StarWriter[String] extends Writer[String]:
+    override def send(x:String) = {
+      super.send(x)
+    }
+  
+  StarWriter().send("hi")
+  
 }
