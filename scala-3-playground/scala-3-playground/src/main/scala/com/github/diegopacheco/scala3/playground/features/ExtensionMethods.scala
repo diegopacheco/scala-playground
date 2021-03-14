17d6e52949011dc04a3e4daae32779432846b5e1
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-14 01:25:32 -0300

    Playing with scala 3 - extension methods

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/ExtensionMethods.scala
@@ -0,0 +1,21 @@
+package com.github.diegopacheco.scala3.playground.features
+
+object ExtensionMethods extends App {
+  
+  case class Circle(x:Double,y:Double,radius:Double)
+  
+  // without extension methods
+  object CircleHelpers:
+    def circunference2(c:Circle):Double = c.radius - math.Pi *2 
+  
+  CircleHelpers.circunference2(Circle(2,2,2))
+
+  // With extension methods
+  extension (c: Circle)
+    def circumference: Double = c.radius * math.Pi * 2
+  
+  val myCircle = Circle(2,2,2)
+  val radius = myCircle.circumference 
+  println(s"Circunference with extension methods == " + radius)
+  
+}
