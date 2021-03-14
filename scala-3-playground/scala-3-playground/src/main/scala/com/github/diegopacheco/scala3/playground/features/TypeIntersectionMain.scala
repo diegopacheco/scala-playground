b57514050c8c0c65c2249cade0b130b88b049495
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-14 00:46:18 -0300

    Playing with scala 3 type intersection

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/TypeIntersectionMain.scala
@@ -0,0 +1,25 @@
+package com.github.diegopacheco.scala3.playground.features
+
+object TypeIntersectionMain extends App{
+
+  trait Resettable:
+    def reset(): Unit
+
+  trait Growable[A]{
+    def add(a: A): Unit  
+  }
+
+  trait Both[A] extends Resettable, Growable[A]
+  
+  def callMe(x:Both[String]): Unit =
+    x.reset()
+    x.add("first")
+
+  class DoItAll extends Both[String]:
+    override def reset()=println("Reseting... ")
+    override def add(a:String)=println(s"adding ${a}")
+  
+  val c:Both[String] = DoItAll()
+  callMe(c)
+  
+}
