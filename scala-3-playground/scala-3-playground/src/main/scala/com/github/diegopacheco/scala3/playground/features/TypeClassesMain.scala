fe79e544ec83a968d8416fdce110b313506497a1
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-14 01:54:27 -0300

    Playing with scala 3 - type classes

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/TypeClassesMain.scala
@@ -0,0 +1,25 @@
+package com.github.diegopacheco.scala3.playground.features
+
+object TypeClassesMain extends App{
+
+  trait Show:
+    def show: String
+
+  trait Showable[A]:
+    extension(a: A) def show: String
+
+  case class Person(firstName: String, lastName: String)
+
+  given Showable[Person] with
+    extension(p: Person) def show: String =
+      s"${p.firstName} ${p.lastName}"
+
+  val person = Person("John", "Doe")
+  println(person.show)
+
+  def showAll[S: Showable](xs: List[S]): Unit =
+    xs.foreach(x => println(x.show))
+
+  showAll(List(Person("Jane","Doe"), Person("Mary","Doe")))
+  
+}
