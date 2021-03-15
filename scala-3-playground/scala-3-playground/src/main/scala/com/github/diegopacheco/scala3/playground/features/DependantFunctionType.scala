bc6590e38bd4b695336cb2b2867c1bab9e4e7b33
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-15 00:41:09 -0300

    Scala 3 - Dependent Function Types ~

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/DependantFunctionType.scala
@@ -0,0 +1,9 @@
+package com.github.diegopacheco.scala3.playground.features
+
+trait Entry { type Key; val key: Key }
+
+def extractKey(e: Entry): e.Key = e.key          // a dependent method
+
+val extractor: (e: Entry) => e.Key = extractKey  // a dependent function value
+//             ^^^^^^^^^^^^^^^^^^^
+//             a dependent function type
