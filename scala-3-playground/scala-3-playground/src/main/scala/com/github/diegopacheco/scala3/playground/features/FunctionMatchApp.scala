20355b27c128101d94d3f64738f38223f941f7c2
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-14 23:39:38 -0300

    return function

---
+++ scala-3-playground/scala-3-playground/src/main/scala/com/github/diegopacheco/scala3/playground/features/FunctionMatchApp.scala
@@ -0,0 +1,16 @@
+package com.github.diegopacheco.scala3.playground.features
+
+@main def FunctionMatchApp():Unit = {
+  
+  def createGreetingFunction(desiredLanguage: String): String => Unit =
+    val englishGreeting = (name: String) => println(s"Hello, $name")
+    val frenchGreeting = (name: String) => println(s"Bonjour, $name")
+    desiredLanguage match
+      case "english" => englishGreeting
+      case "french" => frenchGreeting
+  
+  val result = createGreetingFunction("english");
+  println(result.apply("Diego"))
+  
+} 
+
