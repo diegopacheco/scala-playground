6e07b6f7160f6e2e17807ac09259b7dd7480f667
diegopacheco <diego.pacheco.it@gmail.com>
2022-10-19 19:30:26 -0700

    remove all duplicates scala 3

---
+++ scala3-pattern-tailrec/scala3-remove-duplicates/src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+/* Does not preserve ordering */
+def removeDuplicates[A](xsOuter: List[A]): List[A] = {
+  @annotation.tailrec
+  def removeDuplicatesInner(xs: List[A], collected: List[A]): List[A] = xs match {
+    case Nil => collected
+    case x :: ys => if (collected.contains(x)) removeDuplicatesInner(ys, collected) else removeDuplicatesInner(ys, x :: collected)
+  }
+
+  removeDuplicatesInner(xsOuter, Nil)
+}
+
+def msg = "Removing duplicates"
+def arg = List("a", "a", "a", "b", "a", 1, 2, 3, 4, 1, 1, 1, 5)
+
+@main def hello: Unit =
+  println(msg)
+  println(arg)
+  println(removeDuplicates(arg))
