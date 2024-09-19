b48be22f8d7c96afdbc3544d651612ddd76edd94
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-19 00:33:46 -0700

    scala-3.5-cats-bag-of-cats

--- bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/CatsShow.scala
+++ bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/CatsShow.scala
@@ -3,6 +3,10 @@ package com.github.diegopacheco.bagofcats
 import cats.Show
 import cats.instances.int.*
 
+/**
+ * Show is a type class for types that can be converted to a human readable string.
+ * Show is similar to toString, but it is not a method on the object being converted.
+ */
 object CatsShow extends App{
 
   val showInt:Show[Int] = Show.apply[Int]
