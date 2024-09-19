3c8ce39b3d0681943b7da429de714737f0db1524
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-19 00:33:41 -0700

    scala-3.5-cats-bag-of-cats

--- bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/CatsSemiGroups.scala
+++ bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/CatsSemiGroups.scala
@@ -1,5 +1,8 @@
 package com.github.diegopacheco.bagofcats
 
+/**
+ * Semigroup is anything that can be combined(combine). Cannot be empty.
+ */
 object CatsSemiGroups extends App {
   import cats.Semigroup
   val semigroup = Semigroup[String].combine("Hi ", "there")
