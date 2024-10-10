399100fa42d30d40cb22d9e9d902ee6f31d745ac
diegopacheco <diego.pacheco.it@gmail.com>
2024-10-10 09:20:24 -0700

    bag-of-cats

--- bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/exercises/Printable.scala
+++ bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/exercises/Printable.scala
@@ -1,28 +1,28 @@
 package com.github.diegopacheco.bagofcats.exercises
 
-object Printable extends App{
+trait Printable[A] {
+  def format(value: A): String
+}
 
-  trait Printable[A] {
-    def format(value: A): String
+object PrintableInstances {
+  implicit val stringPrintable = new Printable[String] {
+    def format(input: String) = input
   }
-
-  object PrintableInstances {
-    implicit val stringPrintable = new Printable[String] {
-      def format(input: String) = input
-    }
-    implicit val intPrintable = new Printable[Int] {
-      def format(input: Int) = input.toString
-    }
+  implicit val intPrintable = new Printable[Int] {
+    def format(input: Int) = input.toString
   }
+}
 
-  object Printable {
-    def format[A](input: A)(implicit p: Printable[A]): String =
-      p.format(input)
+object Printable {
+  def format[A](input: A)(implicit p: Printable[A]): String =
+    p.format(input)
 
-    def print[A](input: A)(implicit p: Printable[A]): Unit =
-      println(format(input))
-  }
+  def print[A](input: A)(implicit p: Printable[A]): Unit =
+    println(format(input))
+}
 
+object PrintableApp extends App{
+  
   val a = "Hello"
   val b = 10
 
