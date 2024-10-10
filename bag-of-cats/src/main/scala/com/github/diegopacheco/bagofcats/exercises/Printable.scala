6399057d90b5ad85a8817792ed495ada519522c2
diegopacheco <diego.pacheco.it@gmail.com>
2024-10-10 09:18:55 -0700

    bag-of-cats

--- bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/exercises/Printable.scala
+++ bag-of-cats/src/main/scala/com/github/diegopacheco/bagofcats/exercises/Printable.scala
@@ -1,5 +1,33 @@
 package com.github.diegopacheco.bagofcats.exercises
 
-object Printable {
+object Printable extends App{
+
+  trait Printable[A] {
+    def format(value: A): String
+  }
+
+  object PrintableInstances {
+    implicit val stringPrintable = new Printable[String] {
+      def format(input: String) = input
+    }
+    implicit val intPrintable = new Printable[Int] {
+      def format(input: Int) = input.toString
+    }
+  }
+
+  object Printable {
+    def format[A](input: A)(implicit p: Printable[A]): String =
+      p.format(input)
+
+    def print[A](input: A)(implicit p: Printable[A]): Unit =
+      println(format(input))
+  }
+
+  val a = "Hello"
+  val b = 10
+
+  import PrintableInstances._
+  Printable.print(a)
+  Printable.print(b)
 
 }
