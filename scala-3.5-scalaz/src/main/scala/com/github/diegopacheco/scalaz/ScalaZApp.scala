be2025d815c7fe7c5b7d46c52017c7226eca3506
diegopacheco <diego.pacheco.it@gmail.com>
2024-10-23 21:12:56 -0700

    scala 3.5 scalaz

--- scala-3.5-scalaz/src/main/scala/com/github/diegopacheco/scalaz/ScalaZApp.scala
+++ scala-3.5-scalaz/src/main/scala/com/github/diegopacheco/scalaz/ScalaZApp.scala
@@ -3,6 +3,12 @@ package com.github.diegopacheco.scalaz
 object ScalaZApp extends App{
 
   import scalaz._
+  import Scalaz._
 
+  val res0 = Apply[Option].apply2(some(1), some(2))((a, b) => a + b)
+  println(res0)
+
+  val res1 = Traverse[List].traverse(List(1, 2, 3))(i => some(i))
+  println(res1)
 
 }
