9b66cf055ad2d213648b3bd436964468a54a2b01
diegopacheco <diego.pacheco.it@gmail.com>
2024-10-25 23:51:57 -0700

    .

--- parser-combinator-3.5/src/main/scala/com/github/diegopacheco/parsercombinators/App.scala
+++ parser-combinator-3.5/src/main/scala/com/github/diegopacheco/parsercombinators/App.scala
@@ -1,6 +1,5 @@
 package com.github.diegopacheco.parsercombinators
 
-
 trait Parser[+T] {
   def parse(input: String): Option[(T, String)]
 
@@ -32,76 +31,39 @@ object Parsers {
     if (input.nonEmpty && input.head == c) Some((c, input.tail))
     else None
 
-  def string(s: String): Parser[String] = input =>
-    if (input.startsWith(s)) Some((s, input.drop(s.length)))
+  def number: Parser[Int] = input => {
+    val num = input.takeWhile(_.isDigit)
+    if (num.nonEmpty) Some((num.toInt, input.drop(num.length)))
     else None
-
-  def regex(r: String): Parser[String] = input => {
-    val pattern = r.r
-    pattern.findPrefixOf(input) match {
-      case Some(matched) => Some((matched, input.drop(matched.length)))
-      case None => None
-    }
   }
-
-  def number: Parser[Int] = regex("\\d+").map(_.toInt)
-
-  def whitespace: Parser[String] = regex("\\s*")
 }
 
 object ArithmeticParsers {
   import Parsers._
 
-  def term: Parser[Int] = factor ~ rep1((string("*") ~ factor) | (string("/") ~ factor)) map {
-    case (f, list) => list.foldLeft(f) {
-      case (acc, ("*", v)) => acc * v
-      case (acc, ("/", v)) => acc / v
-    }
+  def factor: Parser[Int] = number
+
+  def term: Parser[Int] = factor ~ opt(char('*') ~ factor) map {
+    case (f, Some((_, v))) => f * v
+    case (f, None) => f
   }
 
-  def expr: Parser[Int] = term ~ rep1((string("+") ~ term) | (string("-") ~ term)) map {
-    case (t, list) => list.foldLeft(t) {
-      case (acc, ("+", v)) => acc + v
-      case (acc, ("-", v)) => acc - v
-    }
+  def expr: Parser[Int] = term ~ opt(char('+') ~ term) map {
+    case (t, Some((_, v))) => t + v
+    case (t, None) => t
   }
 
-  def factor: Parser[Int] = number | (string("(") ~> expr <~ string(")"))
-
-  def rep1[T](p: Parser[T]): Parser[List[T]] = input => {
+  def opt[T](p: Parser[T]): Parser[Option[T]] = input =>
     p.parse(input) match {
-      case Some((value, rest)) =>
-        rep(p).parse(rest) match {
-          case Some((values, rest2)) => Some((value :: values, rest2))
-          case None => Some((List(value), rest))
-        }
-      case None => None
+      case Some((value, rest)) => Some((Some(value), rest))
+      case None => Some((None, input))
     }
-  }
-
-  def rep[T](p: Parser[T]): Parser[List[T]] = input => {
-    def loop(in: String, acc: List[T]): Option[(List[T], String)] = {
-      p.parse(in) match {
-        case Some((value, rest)) => loop(rest, acc :+ value)
-        case None => Some((acc, in))
-      }
-    }
-    loop(input, List())
-  }
-
-  implicit class ParserOps[T](p: Parser[T]) {
-    def |[U >: T](other: Parser[U]): Parser[U] = p | other
-    def ~[U](other: Parser[U]): Parser[(T, U)] = p ~ other
-    def map[U](f: T => U): Parser[U] = p.map(f)
-    def ~>[U](other: Parser[U]): Parser[U] = (p ~ other).map(_._2)
-    def <~[U](other: Parser[U]): Parser[T] = (p ~ other).map(_._1)
-  }
 }
 
 object ParserCombinatorApp extends App {
   import ArithmeticParsers._
 
-  val input = "3 + 5 * (10 - 4)"
+  val input = "3+5*2"
   val result = expr.parse(input)
 
   result match {
