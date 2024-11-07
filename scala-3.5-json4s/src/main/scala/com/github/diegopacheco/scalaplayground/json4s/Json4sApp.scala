0d07ae1a4f1c7dcba934cbd45fab5b6380f615d3
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-07 00:40:00 -0800

    json4s

--- scala-3.5-json4s/src/main/scala/com/github/diegopacheco/scalaplayground/json4s/Json4sApp.scala
+++ scala-3.5-json4s/src/main/scala/com/github/diegopacheco/scalaplayground/json4s/Json4sApp.scala
@@ -9,4 +9,20 @@ object Json4sApp extends App{
 
   val res1 = parse("""{"name":"Toy","price":35.35}""", useBigDecimalForDouble = true)
   println(res1)
+
+  import org.json4s.JsonDSL._
+  import org.json4s.JsonDSL.WithDouble._
+
+  var json = List(1, 2, 3)
+  println(compact(render(json)))
+
+  val json1 = ("name" -> "joe")
+  println(compact(render(json1)))
+
+  val json2 = ("name" -> "joe") ~ ("age" -> 35)
+  println(compact(render(json2)))
+
+  val json3 = ("name" -> "joe") ~ ("age" -> Some(35))
+  println(compact(render(json3)))
+
 }
