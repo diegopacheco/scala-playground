49c4476a83116733e9fbb12d6743f2bcc9c75b80
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-14 00:12:00 -0800

    scala 2.13 + bazel working on intelij

---
+++ scala-2.13-15-bazel/cmd/src/main/scala/com/github/diegopacheco/scalaplayground/cmd/Runner.scala
@@ -0,0 +1,9 @@
+package com.github.diegopacheco.scalaplayground.cmd
+
+import com.github.scalaplayground.lib.Greeting
+
+object Runner {
+    def main(args: Array[String]) = {
+        Greeting.sayHello
+    }
+}
---
+++ scala-2.13-15-bazel/lib/src/main/scala/com/github/scalaplayground/lib/Greeting.scala
@@ -0,0 +1,5 @@
+package com.github.scalaplayground.lib
+
+object Greeting {
+    def sayHello = println("Hello Scala 2.13.15 and Bazel!")
+}
