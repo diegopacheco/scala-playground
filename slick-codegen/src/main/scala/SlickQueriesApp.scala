53cb6523b586f3b0e08c7363e76db1c41f17e429
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-19 00:31:45 -0800

    slick

---
+++ slick-codegen/src/main/scala/SlickQueriesApp.scala
@@ -0,0 +1,29 @@
+
+import demo.Tables.*
+import slick.basic.DatabaseConfig
+import slick.jdbc.H2Profile.api.*
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.duration.*
+import scala.concurrent.Await
+
+object DBSettings {
+  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("slick")
+  val db = dbConfig.db
+}
+
+object SlickQueriesApp extends App{
+  import DBSettings._
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val q = for {
+    co <- Companies
+    cp <- Computers if co.id === cp.manufacturerId && cp.name.like("A%")
+  } yield (co.name, cp.name)
+
+  val resultFuture =db.run(q.result).map { result =>
+    println(result.groupMap(_._1)(_._2).mkString("\n"))
+  }
+
+  Await.result(resultFuture, 10.seconds)
+}
