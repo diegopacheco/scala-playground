a117af8259dc99678eaf2549fcc1a9f7ec2702cb
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-20 00:38:19 -0800

    scala3x

---
+++ scala-3x-playground/src/main/scala/com/github/diegopacheco/scala3x/playground/Union.scala
@@ -0,0 +1,10 @@
+package com.github.diegopacheco.scala3x.playground
+
+object Union extends App:
+  def playstation(version: Int | String): String = version match
+    case i: Int => s"Integer: $i"
+    case s: String => s"String: $s"
+
+  val result1 = playstation(5)     // "Integer: 5"
+  val result2 = playstation("ps4") // "String: ps4"
+  println(s"result1: ${result1} - result2: ${result2}")
