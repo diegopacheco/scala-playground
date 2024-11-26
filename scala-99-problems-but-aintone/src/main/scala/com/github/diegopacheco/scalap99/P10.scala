320fe5b970ede5883e7b344084d55543926074e7
diegopacheco <diego.pacheco.it@gmail.com>
2024-11-26 02:53:28 -0800

    P10 done

---
+++ scala-99-problems-but-aintone/src/main/scala/com/github/diegopacheco/scalap99/P10.scala
@@ -0,0 +1,19 @@
+package com.github.diegopacheco.scalap99
+
+// P10 (*) Run-length encoding of a list.
+//     Use the result of problem P09 to implement the so-called run-length
+//     encoding data compression method.  Consecutive duplicates of elements are
+//     encoded as tuples (N, E) where N is the number of duplicates of the
+//     element E.
+//
+//     Example:
+//     scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+//     res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+object P10:
+  def encode[A](ls: List[A]): List[(Int, A)] = {
+    P09.pack(ls).map( l => (l.size,l.head) )
+  }
+
+object P10Main extends App:
+  import P10._
+  println( encode(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')) )
