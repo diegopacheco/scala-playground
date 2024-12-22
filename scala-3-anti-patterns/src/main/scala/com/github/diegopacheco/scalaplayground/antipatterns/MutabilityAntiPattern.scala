3c6764b198c1e0033ac3c9e186fb461b02ca1960
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-22 03:52:46 -0800

    Scala Anti-Patterns

--- scala-3-anti-patterns/src/main/scala/com/github/diegopacheco/scalaplayground/antipatterns/MutabilityAntiPattern.scala
+++ scala-3-anti-patterns/src/main/scala/com/github/diegopacheco/scalaplayground/antipatterns/MutabilityAntiPattern.scala
@@ -2,8 +2,14 @@ package com.github.diegopacheco.scalaplayground.antipatterns
 
 import scala.collection.mutable.ArrayBuffer
 
+//
+// Anti-Pattern (Unnecessary Mutability)
+// 1. Using mutable variables (var)
+// 2. Using mutable collections
+// 3. Using global variables in functions
+//
 def getLuckyNumbers:ArrayBuffer[Int] = {
-  var numbers = ArrayBuffer[Int]()
+  var numbers = ArrayBuffer[Int]() //Anti-pattern 1 and 2
   numbers += 1
   numbers += 2
   numbers += 3
@@ -17,7 +23,7 @@ object MutabilityAntiPattern extends App{
   private var luckyNumbers: ArrayBuffer[Int] = getLuckyNumbers
   println(luckyNumbers)
 
-  var total = 0 // Anti-pattern
-  val add = (x: Int) => total += x
+  private var total = 0 // Anti-pattern 3
+  private val add = (x: Int) => total += x
   println(add(5))
 }
