1b053e97f435c7e73625d80926d13b84b4698d93
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-22 22:00:24 -0800

    Scala Anti-Patterns

--- scala-3-anti-patterns/src/main/scala/com/github/diegopacheco/scalaplayground/antipatterns/SomeAntiPattern.scala
+++ scala-3-anti-patterns/src/main/scala/com/github/diegopacheco/scalaplayground/antipatterns/SomeAntiPattern.scala
@@ -1,5 +1,10 @@
 package com.github.diegopacheco.scalaplayground.antipatterns
 
+//
+// Some/Option Anti-Patterns
+// 1. Using Some(null) - This is a common mistake, Some(null) is not the same as None.
+// 2. Using get() - This is a common mistake, get() is not safe and can throw a exception.
+//
 def getLastPerson:Option[String] = {
   // imagine people come from DB
   val people = List("Diego","Lucas","Joaquim")
