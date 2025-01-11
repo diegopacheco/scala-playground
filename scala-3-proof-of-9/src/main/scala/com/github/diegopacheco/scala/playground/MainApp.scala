a1ee1b168360b9b57f102c3a74130f0e3d49eb5f
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-10 18:42:26 -0800

    proof of 9

--- scala-3-proof-of-9/src/main/scala/com/github/diegopacheco/scala/playground/MainApp.scala
+++ scala-3-proof-of-9/src/main/scala/com/github/diegopacheco/scala/playground/MainApp.scala
@@ -1,7 +1,6 @@
 package com.github.diegopacheco.scala.playground
 
 def proofOne(): Unit = {
-
   //
   // This function check the result before returning so the check in part of the function.
   //
@@ -15,7 +14,30 @@ def proofOne(): Unit = {
   println(s"Is $testNumber divisible by 9? ${isDivisibleBy9(testNumber)}")
 }
 
+def proofTwo(): Unit = {
+  //
+  // This function also have an embedded check - the IF
+  //
+  def printOdd(n: Int): Unit = {
+    if (n % 2 != 0) println(s"$n is odd")
+  }
+  (1 to 2).foreach(printOdd)
+}
+
+def proofThree(): Unit = {
+  //
+  // This function also have an embedded check - the assert (Exception is throw if fail)
+  //
+  def printOdd(n: Int): Unit = {
+    assert(n % 2 == 0)
+    println(s"$n is even")
+  }
+  (2 to 2).foreach(printOdd)
+}
+
 object MainApp extends App{
   proofOne()
+  proofTwo()
+  proofThree()
 }
 
