95faf354168b9f7c9458e1469fab1fc6381a4783
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-10 18:38:10 -0800

    proof of 9

--- scala-3-proof-of-9/src/main/scala/com/github/diegopacheco/scala/playground/MainApp.scala
+++ scala-3-proof-of-9/src/main/scala/com/github/diegopacheco/scala/playground/MainApp.scala
@@ -1,7 +1,10 @@
 package com.github.diegopacheco.scala.playground
 
-object MainApp extends App{
+def proofOne(): Unit = {
 
+  //
+  // This function check the result before returning so the check in part of the function.
+  //
   def isDivisibleBy9(number: Int): Boolean = {
     val sumOfDigits = number.toString.map(_.asDigit).sum
     sumOfDigits % 9 == 0
@@ -10,6 +13,9 @@ object MainApp extends App{
   // Test the function
   val testNumber = 123456
   println(s"Is $testNumber divisible by 9? ${isDivisibleBy9(testNumber)}")
+}
 
+object MainApp extends App{
+  proofOne()
 }
 
