66c69ed1d0523c99cd49dd4ebb8789e0b6f8ef5a
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-14 00:15:48 -0800

    scala 3x ids poc - added login service

--- scala-3-6-ids-poc/build.sbt
+++ scala-3-6-ids-poc/build.sbt
@@ -1,3 +1,5 @@
 name := "scala-3-6-ids-poc"
 version := "1.0"
 scalaVersion := "3.6.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test"
---
+++ scala-3-6-ids-poc/src/test/scala/com/github/diegopacheco/scala/ids/loginservice/LoginTest.scala
@@ -0,0 +1,23 @@
+package com.github.diegopacheco.scala.ids.loginservice
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class LoginTest extends AnyFunSuite {
+
+  test("Login with correct credentials should return true") {
+    assert(LoginService.login(10_001, hash("banana")))
+    assert(LoginService.login(10_002, hash("apple")))
+    assert(LoginService.login(10_003, hash("orange")))
+  }
+
+  test("Login with incorrect credentials should return false") {
+    assert(!LoginService.login(10_001, hash("wrongpassword")))
+    assert(!LoginService.login(10_004, hash("banana")))
+  }
+
+  private def hash(s: String): String = {
+    val md = java.security.MessageDigest.getInstance("SHA-512")
+    val digest = md.digest(s.getBytes)
+    digest.map("%02x".format(_)).mkString
+  }
+}
