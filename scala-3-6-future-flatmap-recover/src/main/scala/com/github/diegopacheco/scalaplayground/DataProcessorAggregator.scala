07c2efd74160f4b31193cb134088d13f28606e6c
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-17 14:59:12 -0800

    recover poc

--- scala-3-6-future-flatmap-recover/src/main/scala/com/github/diegopacheco/scalaplayground/DataProcessorAggregator.scala
+++ scala-3-6-future-flatmap-recover/src/main/scala/com/github/diegopacheco/scalaplayground/DataProcessorAggregator.scala
@@ -8,14 +8,15 @@ object DataProcessorAggregator {
 
   def process(ids: List[Int]): Future[List[Int]] = {
     val futureResults = for {
-      result <- processor.process(ids).flatMap {
-        case Left(value) => Future.successful(value)
-        case Right(_) => Future.successful(List.empty[Int])
-      }.recoverWith {
-        case e: Any =>
-          println("[Recover] Got this: " + e)
-          Future.successful(List.empty[Int])
-      }
+      result <- processor.process(ids).
+        flatMap {
+          case Left(value) => Future.successful(value)
+          case Right(_) => Future.successful(List.empty[Int])
+        }.recoverWith {
+          case e: Any =>
+            println("[Recover] Got this: " + e)
+            Future.successful(List.empty[Int])
+        }
     } yield result
     futureResults
   }
