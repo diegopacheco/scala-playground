e484fb2060f05c14c8976360e7dd94560573cfa9
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-18 00:21:49 -0800

    combine poc

--- scala-3-6-future-combine/src/main/scala/com/github/diegopacheco/scalaplayground/NumberGenerator.scala
+++ scala-3-6-future-combine/src/main/scala/com/github/diegopacheco/scalaplayground/NumberGenerator.scala
@@ -4,10 +4,19 @@ import scala.concurrent.{Future, ExecutionContext}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object NumberGenerator {
+
   def even: Future[List[Int]] = Future {
-    LazyList.from(0, 2).takeWhile(_ => true).toList
+    LazyList.from(0, 2).take(100).toList
   }
+
   def odd: Future[List[Int]] = Future {
-    LazyList.from(1, 2).takeWhile(_ => true).toList
+    LazyList.from(1, 2).take(100).toList
+  }
+
+  def combined: Future[List[Int]] = {
+    for {
+      evens <- even
+      odds <- odd
+    } yield (evens ++ odds).sorted
   }
 }
--- scala-3-6-future-combine/src/test/scala/com/github/diegopacheco/scalaplayground/NumberGeneratorTest.scala
+++ scala-3-6-future-combine/src/test/scala/com/github/diegopacheco/scalaplayground/NumberGeneratorTest.scala
@@ -1,3 +1,26 @@
 package com.github.diegopacheco.scalaplayground
 
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+import scala.concurrent.Future
 
+class NumberGeneratorTest extends AsyncFlatSpec with Matchers {
+
+  "NumberGenerator" should "generate an infinite list of even numbers" in {
+    NumberGenerator.even.map { evens =>
+      evens.take(10) shouldEqual List(0, 2, 4, 6, 8, 10, 12, 14, 16, 18)
+    }
+  }
+
+  it should "generate an infinite list of odd numbers" in {
+    NumberGenerator.odd.map { odds =>
+      odds.take(10) shouldEqual List(1, 3, 5, 7, 9, 11, 13, 15, 17, 19)
+    }
+  }
+
+  it should "combine even and odd numbers into a sorted list" in {
+    NumberGenerator.combined.map { combined =>
+      combined.take(20) shouldEqual List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+    }
+  }
+}
