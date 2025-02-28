f21218c86dda3cb8383cbe974fd059e788cbac8e
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-28 00:26:20 -0800

    monad 2

--- scala-3-left-right-exceptions/src/main/scala/ValidMonadTake2.scala
+++ scala-3-left-right-exceptions/src/main/scala/ValidMonadTake2.scala
@@ -28,3 +28,23 @@ object RightS {
   def apply[A](value: A): RightS[A] = new RightS(value)
   def safe[A](value: A): EitherS[Any, A] = new RightS(value)
 }
+
+object ValidMonadMain2 extends App {
+  val safeEitherString = EitherS(null)
+  safeEitherString match {
+    case LeftS(value) => println(s"LEFT safeEitherString: LeftS($value)")
+    case RightS(value) => println(s"RIGHT safeEitherString: RightS($value)")
+  }
+
+  val safeEitherException = EitherS(throw new Exception("Exception"))
+  safeEitherException match {
+    case LeftS(value) => println(s"LEFT safeEitherException: LeftS($value)")
+    case RightS(value) => println(s"RIGHT safeEitherException: RightS($value)")
+  }
+
+  val safeEitherInt = EitherS(10).flatMap(x => EitherS(x * 2))
+  safeEitherInt match {
+    case LeftS(value) => println(s"LEFT safeEitherInt: LeftS($value)")
+    case RightS(value) => println(s"RIGHT safeEitherInt: RightS($value)")
+  }
+}
