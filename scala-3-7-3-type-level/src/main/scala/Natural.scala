a8ec3b903c856eccbd36055422493e64c645d39e
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-20 12:56:22 -0700

    typelevel scala 3.7.x and java 25

--- scala-3-7-3-type-level/src/main/scala/Natural.scala
+++ scala-3-7-3-type-level/src/main/scala/Natural.scala
@@ -1,6 +1,15 @@
-sealed trait Nat
-case object Zero extends Nat
-case class Succ[N <: Nat](n: N) extends Nat
+sealed trait Nat {
+  def toInt: Int
+  override def toString: String = toInt.toString
+}
+
+case object Zero extends Nat {
+  def toInt: Int = 0
+}
+
+case class Succ[N <: Nat](n: N) extends Nat {
+  def toInt: Int = 1 + n.toInt
+}
 
 type One = Succ[Zero.type]
 type Two = Succ[One]
@@ -13,6 +22,6 @@ type Add[A <: Nat, B <: Nat] <: Nat = A match
 object Natural {
   def runNatural(): Unit = {
     val result: Add[Two, One] = Succ(Succ(Succ(Zero)))
-    println(result)
+    println(result) // 3
   }
 }
