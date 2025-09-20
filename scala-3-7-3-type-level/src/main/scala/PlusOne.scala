abe4b69c343a01d36e47d20d1aa50ec759466f3b
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-20 13:51:49 -0700

    typelevel scala 3.7.x and java 25

--- scala-3-7-3-type-level/src/main/scala/PlusOne.scala
+++ scala-3-7-3-type-level/src/main/scala/PlusOne.scala
@@ -1,15 +1,6 @@
-type Inc[N] = N match
-  case 0 => 1
-  case 1 => 2
-  case 2 => 3
-  case 3 => 4
-  case 4 => 5
-  case 5 => 6
-  case 6 => 7
-  case 7 => 8
-  case 8 => 9
-  case 9 => 10
-  case 10 => 11
+import scala.compiletime.ops.int.*
+
+type Inc[N <: Int] = N + 1
 
 type Plus1[T] = T match
   case EmptyTuple => EmptyTuple
