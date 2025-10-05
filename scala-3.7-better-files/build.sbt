82ea5a229f61d06347dde5dc12b7b67eb829a6e0
diegopacheco <diego.pacheco.it@gmail.com>
2025-10-04 20:20:53 -0700

    added scala 3 + better files

--- scala-3.7-better-files/build.sbt
+++ scala-3.7-better-files/build.sbt
@@ -1,3 +1,5 @@
 name := "scala-3.7-better-files"
 version := "1.0"
 scalaVersion := "3.7.2"
+
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.9.2"
--- scala-3.7-better-files/src/main/scala/Main.scala
+++ scala-3.7-better-files/src/main/scala/Main.scala
@@ -1,2 +1,17 @@
-@main def main():Unit =
-  println("Scala 3x Rocks!")
+import better.files.*
+
+@main def main(): Unit =
+  val file = File("test.txt")
+
+  file.write("Hello from Scala 3.7 with better-files!")
+  println(s"Written to: ${file.path}")
+
+  val content = file.contentAsString
+  println(s"Content: $content")
+
+  val dir = File(".")
+  println(s"\nFiles in current directory:")
+  dir.list.take(5).foreach(f => println(s"  ${f.name}"))
+
+  file.delete()
+  println(s"\nDeleted: ${file.path}")
