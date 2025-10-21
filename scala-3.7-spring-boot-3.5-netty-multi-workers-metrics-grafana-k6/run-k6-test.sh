6b99e130e0f446b76591bc518855894f25a5cc77
diegopacheco <diego.pacheco.it@gmail.com>
2025-10-20 21:56:09 -0700

    progress

--- scala-3.7-spring-boot-3.5-netty-multi-workers-metrics-grafana-k6/run-k6-test.sh
+++ scala-3.7-spring-boot-3.5-netty-multi-workers-metrics-grafana-k6/run-k6-test.sh
@@ -3,5 +3,5 @@
 echo "Running k6 load test with Prometheus Remote Write..."
 
 K6_PROMETHEUS_RW_SERVER_URL=http://localhost:9090/api/v1/write \
-K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true \
+K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=false \
 k6 run -o experimental-prometheus-rw k6-load-test.js
